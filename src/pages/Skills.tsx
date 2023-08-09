b057a8850fe45d5f239d2fce9f7beebda21a252c
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-09 12:35:01 -0600

    styled

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,3 +1,25 @@
+import { styled } from "@mui/material";
+import { SkillBubble } from "../components/SkillBubble";
+import { SKILLS } from "../lib";
+
+const SkillContainer = styled("section")(({ theme }) => ({
+  display: "flex",
+  flexWrap: "wrap",
+  alignContent: "center",
+}));
+
 export const Skills = () => {
-  return <section id="skills"></section>;
+  return (
+    <SkillContainer id="skills">
+      {SKILLS.sort((a, b) => {
+        const lowerA = a.name.toLowerCase();
+        const lowerB = b.name.toLowerCase();
+        if (lowerA < lowerB) return -1;
+        if (lowerA > lowerB) return 1;
+        return 0;
+      }).map((s) => (
+        <SkillBubble key={s.name} skill={s} />
+      ))}
+    </SkillContainer>
+  );
 };
