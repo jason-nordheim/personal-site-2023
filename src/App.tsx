4a666ab6fa0fded735936d6de027c319a4a4da41
Jason Nordheim <jnordheim@Apollo.local>
2023-08-09 08:52:48 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography variant="h2">Jason Nordheim</Typography>
+    </>
+  );
+}
+
+export default App;
