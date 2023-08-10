014d02fb79cb73bc60aa71213c28bba472300152
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-10 12:39:04 -0600

    added profile image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,10 @@
 import { css } from "../styled-system/css";
+import profile from "../assets/profile.jpeg";
 
 export const Hero = () => {
-  return <section className={css({ my: 2, px: 1, py: 1, h: "10", w: "10" })}>"Hero Placeholder</section>;
+  return (
+    <section className={css({ my: 2, px: 1, py: 1, placeContent: "center", display: "flex" })}>
+      <img className={css({ height: "300px", rounded: "lg" })} src={profile} alt="jason nordheim" />
+    </section>
+  );
 };
