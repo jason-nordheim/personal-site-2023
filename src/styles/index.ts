22a756efbc55fc5a3282f7028970019745f6f816
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-10 14:33:38 -0600

    updated section style

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -3,7 +3,18 @@ import { container } from "../styled-system/patterns";
 import { SystemStyleObject } from "../styled-system/types";
 
 export const section = (styles: SystemStyleObject) =>
-  container({ my: 2, px: 1, py: 1, w: "100%", mb: "10", ...styles });
+  container({
+    my: 2,
+    px: 1,
+    py: 1,
+    w: "100%",
+    mb: "10",
+    bg: "gray.100",
+    border: "1px solid black",
+    rounded: "md",
+    m: "10px",
+    ...styles,
+  });
 export const sectionTitle = (styles: SystemStyleObject) =>
   css({ textAlign: "center", textStyle: "2xl", fontFamily: "serif", ...styles });
 export const sectionSubTitle = (styles: SystemStyleObject) => css({ fontSize: "md", ...styles });
