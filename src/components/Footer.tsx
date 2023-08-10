d574f95ed648d51a3e36be9902a35ede4467d069
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-10 13:04:41 -0600

    removed file, consolidate components, and updated styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,24 @@
+import { FC } from "react";
 import { css } from "../styled-system/css";
-import { FooterLink } from "./FooterLink";
+
+type FooterLinkProps = {
+  href: string;
+};
+
+export const FooterLink: FC<React.PropsWithChildren<FooterLinkProps>> = ({ href, children }) => {
+  return (
+    <li>
+      <a className={css({ color: "blue.500", textDecoration: "none" })} href={href}>
+        {children}
+      </a>
+    </li>
+  );
+};
 
 export const Footer = () => {
   return (
     <footer>
-      <ul className={css({ display: "flex" })}>
+      <ul className={css({ display: "flex", justifyContent: "space-evenly", bg: "gray.200", mt: "10px", py: "10px" })}>
         <FooterLink href="#">Github</FooterLink>
         <FooterLink href="#">LinkedIn</FooterLink>
         <FooterLink href="#">Email</FooterLink>
