6b1e98231c80cf286e932c22d856635b4e7dd693
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-11 17:41:42 -0600

    removed commented out code

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,6 +10,7 @@ export const Navigation: FC<TabProps> = ({ tab, onTabChange }) => {
         position: "sticky",
         display: "flex",
         flexDirection: "row",
+
         bg: "blue.100",
         smDown: {
           height: "50px",
@@ -17,25 +18,6 @@ export const Navigation: FC<TabProps> = ({ tab, onTabChange }) => {
         },
       })}
     >
-      {/* <h1
-        className={css({
-          display: "flex",
-          placeContent: "center",
-          fontSize: "4xl",
-          lineHeight: "loose",
-          fontFamily: "monospace",
-          alignSelf: "center",
-          color: "white",
-          aspectRatio: "square",
-          borderRadius: "50%",
-          bg: "blue.500",
-          width: "74px",
-          border: "1px solid black",
-          m: "5px",
-        })}
-      >
-        JN
-      </h1> */}
       <div
         className={css({
           smDown: {
