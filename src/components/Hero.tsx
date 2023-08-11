11f5420e7d3c60fe47852e385c4bb64365116d5e
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-10 18:00:12 -0600

    nade circular

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,17 @@ import profile from "../assets/profile.jpeg";
 export const Hero = () => {
   return (
     <section className={css({ my: 2, px: 1, py: 1, placeContent: "center", display: "flex" })}>
-      <img className={css({ height: "300px", rounded: "lg" })} src={profile} alt="jason nordheim" />
+      <div
+        className={css({
+          height: "200px",
+          width: "200px",
+          overflow: "hidden",
+          rounded: "50%",
+          border: "1px solid black",
+        })}
+      >
+        <img src={profile} alt="jason nordheim" height="350px" className={css({ translate: "0px 70px", mt: "-20" })} />
+      </div>
     </section>
   );
 };
