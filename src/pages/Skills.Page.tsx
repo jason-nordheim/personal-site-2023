9e66302ac0e00f36f7294aecb34fccb187188bba
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-12 04:31:59 -0600

    seperated into components

--- src/pages/Skills.Page.tsx
+++ src/pages/Skills.Page.tsx
@@ -1,5 +1,14 @@
-import { SKILL_CATEGORY } from "../lib";
-import { useState } from "react";
+import { SKILL_CATEGORY, Skill } from "../lib";
+import {
+  AnchorHTMLAttributes,
+  DetailedHTMLProps,
+  FC,
+  FormHTMLAttributes,
+  HTMLAttributes,
+  LabelHTMLAttributes,
+  PropsWithChildren,
+  useState,
+} from "react";
 import { container } from "../styled-system/patterns";
 import { css } from "../styled-system/css";
 import { AiOutlineArrowDown } from "react-icons/ai";
@@ -11,10 +20,151 @@ import {
   SORT_OPTIONS,
   SortOptions,
   TagFilters,
-  capitalizeFirstChar,
   makeSkillStyles,
 } from "./Skills.Lib";
 import { useSortedAndFilteredSkills } from "./Skill.hooks";
+import { capitalizeFirstChar } from "../lib/utils/strings";
+
+type FormProps = DetailedHTMLProps<FormHTMLAttributes<HTMLFormElement>, HTMLFormElement>;
+type DivProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+type LabelProps = DetailedHTMLProps<LabelHTMLAttributes<HTMLLabelElement>, HTMLLabelElement>;
+type AnchorProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
+
+const SkillsForm: FC<PropsWithChildren<FormProps>> = ({ children, ...rest }) => {
+  return (
+    <form
+      className={css({ display: "flex", flexDirection: "row", fontFamily: "monospace", gap: "10px", my: "10px" })}
+      {...rest}
+    >
+      {children}
+    </form>
+  );
+};
+
+const SortAndFilterColumn: FC<PropsWithChildren<DivProps>> = ({ children, ...rest }) => {
+  return (
+    <div
+      className={css({
+        sm: {
+          width: "50%",
+        },
+        md: {
+          width: "20%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        },
+      })}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
+
+const TagSelectionColumn: FC<PropsWithChildren<DivProps>> = ({ children, ...rest }) => {
+  return (
+    <div
+      className={css({
+        display: "flex",
+        rounded: "md",
+        flexDirection: "column",
+        border: "1px solid gray",
+        flexGrow: 1,
+      })}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
+
+const SortFilterContainer: FC<PropsWithChildren<DivProps>> = ({ children, ...rest }) => {
+  return (
+    <div
+      className={css({
+        smDown: { mb: "2" },
+        display: "flex",
+        flexDirection: "column",
+        rounded: "md",
+        border: "1px solid gray",
+      })}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
+
+const SortFilterLabel: FC<PropsWithChildren<LabelProps>> = ({ children, ...rest }) => {
+  return (
+    <label
+      className={css({
+        textAlign: "center",
+        borderBottom: "1px solid black",
+        w: "100%",
+        fontWeight: "bold",
+        bg: "blue.300",
+      })}
+      {...rest}
+    >
+      {children}
+    </label>
+  );
+};
+
+const TagSelectionContainer: FC<PropsWithChildren<DivProps>> = ({ children, ...rest }) => {
+  return (
+    <div
+      className={css({
+        smDown: {
+          my: "5px",
+          display: "grid",
+          gridTemplateColumns: "1",
+          "& span": {
+            "& span": { ml: "2" },
+            "& input": { ml: "2" },
+          },
+        },
+        md: { ml: "10px", display: "grid", gridTemplateColumns: 3 },
+      })}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
+
+const SkillBubbleContainer: FC<PropsWithChildren<DivProps>> = ({ children, ...rest }) => {
+  return (
+    <div
+      className={container({
+        display: "flow",
+        textAlign: "center",
+        flexFlow: "row",
+        opacity: 0.8,
+        rounded: "md",
+        lineHeight: "tight",
+      })}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
+
+type SkillBubbleProps = FC<AnchorProps & { skill: Skill }>;
+
+const SkillBubble: SkillBubbleProps = ({ skill, ...rest }) => {
+  return (
+    <a href={skill.href} key={skill.name} {...rest}>
+      <span className={css({ display: "flex", alignItems: "center", justifyContent: "center" })}>
+        {skill.icon && <skill.icon className={css({ mr: "5px", fontSize: "lg" })} />}
+        <span>{skill.name}</span>
+      </span>
+    </a>
+  );
+};
 
 export const SkillsPage = () => {
   const [filters, setFilters] = useState<TagFilters>(FILTERS_ALL);
@@ -40,71 +190,17 @@ export const SkillsPage = () => {
   return (
     <PageContainer>
       <PageTitle>Skills</PageTitle>
-      <form
-        className={css({ display: "flex", flexDirection: "row", fontFamily: "monospace", gap: "10px", my: "10px" })}
-      >
-        <div
-          className={css({
-            sm: {
-              width: "50%",
-            },
-            md: {
-              width: "20%",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-            },
-          })}
-        >
-          <div
-            className={css({
-              smDown: { mb: "2" },
-
-              display: "flex",
-              flexDirection: "column",
-              rounded: "md",
-              border: "1px solid gray",
-            })}
-          >
-            <label
-              className={css({
-                textAlign: "center",
-                borderBottom: "1px solid black",
-                w: "100%",
-                fontWeight: "bold",
-                bg: "blue.300",
-              })}
-              htmlFor="sortOption"
-            >
-              Sort by:
-            </label>
+      <SkillsForm>
+        <SortAndFilterColumn>
+          <SortFilterContainer>
+            <SortFilterLabel htmlFor="sortOption">Sort by:</SortFilterLabel>
             <select id="sortOption" value={sortField} onChange={(e) => setSortField(e.target.value as SortOptions)}>
               <option value={SORT_OPTIONS.name}>{capitalizeFirstChar(SORT_OPTIONS.name)}</option>
               <option value={SORT_OPTIONS.category}>{capitalizeFirstChar(SORT_OPTIONS.category)}</option>
             </select>
-          </div>
-          <div
-            className={css({
-              smDown: { mt: "4" },
-              bg: "gray.100",
-              display: "flex",
-              flexDirection: "column",
-              rounded: "md",
-              border: "1px solid gray",
-            })}
-          >
-            <label
-              className={css({
-                textAlign: "center",
-                borderBottom: "1px solid black",
-                w: "100%",
-                fontWeight: "bold",
-                bg: "blue.300",
-              })}
-              htmlFor="filterBy"
-            >
-              Filter by:
-            </label>
+          </SortFilterContainer>
+          <SortFilterContainer>
+            <SortFilterLabel htmlFor="filterBy">Filter by:</SortFilterLabel>
             <select
               id="filterBy"
               value={filterCategory}
@@ -123,43 +219,11 @@ export const SkillsPage = () => {
                 }),
               ]}
             </select>
-          </div>
-        </div>
-        <div
-          className={css({
-            display: "flex",
-            rounded: "md",
-            flexDirection: "column",
-            border: "1px solid gray",
-            flexGrow: 1,
-          })}
-        >
-          <label
-            className={css({
-              textAlign: "center",
-              borderBottom: "1px solid black",
-              w: "100%",
-              fontWeight: "bold",
-              bg: "blue.300",
-            })}
-            htmlFor="filterOptions"
-          >
-            Show skills related to:
-          </label>
-          <div
-            className={css({
-              smDown: {
-                my: "5px",
-                display: "grid",
-                gridTemplateColumns: "1",
-                "& span": {
-                  "& span": { ml: "2" },
-                  "& input": { ml: "2" },
-                },
-              },
-              md: { ml: "10px", display: "grid", gridTemplateColumns: 3 },
-            })}
-          >
+          </SortFilterContainer>
+        </SortAndFilterColumn>
+        <TagSelectionColumn>
+          <SortFilterLabel htmlFor="filterOptions">Show skills related to:</SortFilterLabel>
+          <TagSelectionContainer>
             {[
               ...Object.keys(filters).map((k) => {
                 // key == tag
@@ -171,9 +235,9 @@ export const SkillsPage = () => {
                 );
               }),
             ]}
-          </div>
-        </div>
-      </form>
+          </TagSelectionContainer>
+        </TagSelectionColumn>
+      </SkillsForm>
       <div
         className={css({
           display: "flex",
@@ -189,28 +253,12 @@ export const SkillsPage = () => {
         <span className={css({ px: "2" })}>Click any skill below for more information</span>
         <AiOutlineArrowDown />
       </div>
-      <div
-        className={container({
-          display: "flow",
-          textAlign: "center",
-          flexFlow: "row",
-          opacity: 0.8,
-          rounded: "md",
-          lineHeight: "tight",
-        })}
-      >
+      <SkillBubbleContainer>
         {skills.map((s) => {
           const styles = makeSkillStyles(s.category);
-          return (
-            <a href={s.href} key={s.name} className={styles}>
-              <span className={css({ display: "flex", alignItems: "center", justifyContent: "center" })}>
-                {s.icon && <s.icon className={css({ mr: "5px", fontSize: "lg" })} />}
-                <span>{s.name}</span>
-              </span>
-            </a>
-          );
+          return <SkillBubble key={s.name} className={styles} skill={s} />;
         })}
-      </div>
+      </SkillBubbleContainer>
     </PageContainer>
   );
 };
