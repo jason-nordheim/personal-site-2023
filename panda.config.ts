a0a91f079fb969f49e11900b196a13957d408470
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-12 04:34:16 -0600

    started creating pattern

--- panda.config.ts
+++ panda.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "@pandacss/dev";
+import { SKILL_CATEGORY } from "./src/lib/skills";
 
 export default defineConfig({
   // Whether to use css reset
@@ -13,4 +14,37 @@ export default defineConfig({
 
   // The output directory for your css system
   outdir: "src/styled-system",
+  theme: {
+    extend: {
+      recipes: {
+        skillBubble: {
+          description: "custom styles for skill buttons",
+          base: {
+            display: "inline-block",
+            fontSize: "sm",
+            borderRadius: "md",
+            border: "1px solid black",
+            fontFamily: "monospace",
+            fontStretch: "condensed",
+            margin: "10px",
+            minWidth: "100px",
+            p: "5px 10px",
+          },
+          defaultVariants: {
+            variant: SKILL_CATEGORY.Concept,
+          },
+          variants: {
+            variant: {
+              [SKILL_CATEGORY.Concept]: { backgroundColor: "yellow.200" },
+              [SKILL_CATEGORY.Framework]: { backgroundColor: "red.300" },
+              [SKILL_CATEGORY.Language]: { backgroundColor: "blue.300" },
+              [SKILL_CATEGORY.Library]: { backgroundColor: "green.300" },
+              [SKILL_CATEGORY.Software]: { backgroundColor: "orange.300" },
+              [SKILL_CATEGORY.Technology]: { backgroundColor: "amber.300" },
+            },
+          },
+        },
+      },
+    },
+  },
 });
