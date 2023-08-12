7e532cb50f72f985acb7e1cc0e97d36e0ca0e413
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-12 04:59:45 -0600

    added new elements

--- src/pages/common/ElementProps.ts
+++ src/pages/common/ElementProps.ts
@@ -10,3 +10,6 @@ export type FormProps = DetailedHTMLProps<FormHTMLAttributes<HTMLFormElement>, H
 export type DivProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 export type LabelProps = DetailedHTMLProps<LabelHTMLAttributes<HTMLLabelElement>, HTMLLabelElement>;
 export type AnchorProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
+export type PProps = DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>;
+export type HTMLElementProps = DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
+export type HeadingProps = DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>;
