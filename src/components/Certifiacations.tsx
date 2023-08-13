4893ebc55b48d6ded136295ed01d626069e17282
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-13 12:31:57 -0600

    started certifications component

---
+++ src/components/Certifiacations.tsx
@@ -0,0 +1,9 @@
+import { PageTitle } from "../pages/common";
+
+export const Certifications = () => {
+  return (
+    <section id="certifications">
+      <PageTitle>Certifications</PageTitle>
+    </section>
+  );
+};
