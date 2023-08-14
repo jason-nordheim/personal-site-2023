20197a6e0222b621f1729b74ca07633d3efe6b70
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-14 13:31:43 -0600

    updated barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ export * from "./Navigation";
 export * from "./Hero";
 export * from "./common";
 export * from "./CardContainer";
+export * from "./CardGrid";
 export * from "./sections/CertificationsSection";
 export * from "./Tabs";
