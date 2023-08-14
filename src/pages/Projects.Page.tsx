594d71b158d7b6c99ea236208c15f4d1ba335796
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-14 13:08:43 -0600

    basic setup

--- src/pages/Projects.Page.tsx
+++ src/pages/Projects.Page.tsx
@@ -1,57 +1,101 @@
+import { FC } from "react";
 import { Caption, CardContainer, PageContainer, PageTitle, SubTitle } from "../components";
 import { CardGrid } from "../components/CardGrid";
+import { AiFillGithub } from "react-icons/ai";
+import { MdOutlinePreview } from "react-icons/md";
+import { css } from "../styled-system/css";
 
-const PROJECTS = [
+type Project = {
+  title: string;
+  liveDemoUrl?: string;
+  repoUrl?: string;
+  caption: string;
+};
+
+const PROJECTS: Project[] = [
   {
-    title: "2048",
-    link: "https://play-2048-with-me.web.app/",
-    links: [
-      { description: "demo", url: "https://play-2048-with-me.web.app/" },
-      {
-        description: "rpo",
-        url: "https://github.com/jason-nordheim/react-2048",
-      },
-    ],
-    desc: "The classic game of combining tiles to get to 2048",
-    alt: "React 2048",
+    title: "React 2048",
+    liveDemoUrl: "https://play-2048-with-me.web.app/",
+    repoUrl: "https://github.com/jason-nordheim/react-2048",
+    caption: "The classic game of combining tiles to get to 2048",
   },
   {
-    title: "Snake",
-    links: [
-      { description: "demo", url: "https://vanilla-js-snake.web.app/" },
-      {
-        description: "repo",
-        url: "https://github.com/jason-nordheim/pure-js-snake",
-      },
-    ],
-    link: "https://vanilla-js-snake.web.app/",
-    desc: "Checkout the classic arcade game snake, built with CSS Grid and vanilla JavaScript",
-    alt: "Pure JavaScript Snake",
+    title: "Pure JavaScript Snake",
+    liveDemoUrl: "https://vanilla-js-snake.web.app/",
+    repoUrl: "https://github.com/jason-nordheim/pure-js-snake",
+    caption: "Checkout the classic arcade game snake, built with CSS Grid and vanilla JavaScript",
   },
   {
     title: "Chatter One",
-    links: [
-      { description: "demo", url: "https://chatter-one.netlify.app/" },
-      {
-        description: "repo",
-        url: "https://github.com/jason-nordheim/chatter-one",
-      },
-    ],
-    link: "https://github.com/jason-nordheim/chatter-one",
-    desc: "A simple chat application with a UI similar to Microsoft Teams, built using ChatEngine.io",
-    alt: "Chatter One Login",
+    liveDemoUrl: "https://chatter-one.netlify.app/",
+    repoUrl: "https://github.com/jason-nordheim/chatter-one",
+    caption: "A simple chat application with a UI similar to Microsoft Teams, built using ChatEngine.io",
   },
 ];
+
+type ProjectSubtitleProps = {
+  projectName: string;
+  repoUrl?: string;
+  liveDemoUrl?: string;
+};
+
+const ProjectSubtitle: FC<ProjectSubtitleProps> = ({ projectName, repoUrl, liveDemoUrl }) => {
+  const linkStyles = css({
+    _hover: {
+      color: "blue.500",
+      fontWeight: "semibold",
+    },
+  });
+  return (
+    <>
+      <SubTitle className={css({ display: "flex", justifyContent: "space-between" })}>
+        <span>{projectName}</span>
+        <span className={css({ display: "flex", pt: "1", alignContent: "center", gap: "1" })}>
+          {repoUrl?.length ? (
+            <a className={linkStyles} href={repoUrl} aria-label={`${projectName} repo`}>
+              <AiFillGithub />
+            </a>
+          ) : null}
+          {liveDemoUrl?.length ? (
+            <a className={linkStyles} href={liveDemoUrl} aria-label={`${projectName} live demo`}>
+              <MdOutlinePreview />
+            </a>
+          ) : null}
+        </span>
+      </SubTitle>
+    </>
+  );
+};
+
+type ProjectCardProps = ProjectSubtitleProps & {
+  caption: string;
+};
+
+const ProjectCard: FC<ProjectCardProps> = ({ caption, projectName, liveDemoUrl, repoUrl }) => {
+  return (
+    <CardContainer>
+      <ProjectSubtitle projectName={projectName} liveDemoUrl={liveDemoUrl} repoUrl={repoUrl} />
+      <Caption>{caption}</Caption>
+    </CardContainer>
+  );
+};
 
 export const ProjectsPage = () => {
   return (
     <PageContainer>
       <PageTitle>Projects</PageTitle>
       <CardGrid>
-        <CardContainer>
-          <SubTitle>2048</SubTitle>
-          <Caption>Simple Implementation of the classic 2048 grid game using React</Caption>
-        </CardContainer>
+        {PROJECTS.map((p) => {
+          return (
+            <ProjectCard
+              key={p.title}
+              caption={p.caption}
+              projectName={p.title}
+              liveDemoUrl={p.liveDemoUrl}
+              repoUrl={p.repoUrl}
+            />
+          );
+        })}
       </CardGrid>
     </PageContainer>
   );
