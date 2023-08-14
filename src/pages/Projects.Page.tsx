a4ba1f8fe180af8642f6ba8df474317584ee38a0
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-14 13:29:03 -0600

    added technologies

--- src/pages/Projects.Page.tsx
+++ src/pages/Projects.Page.tsx
@@ -4,12 +4,14 @@ import { CardGrid } from "../components/CardGrid";
 import { AiFillGithub } from "react-icons/ai";
 import { MdOutlinePreview } from "react-icons/md";
 import { css } from "../styled-system/css";
+import { sortAscending } from "../lib/utils";
 
 type Project = {
   title: string;
   liveDemoUrl?: string;
   repoUrl?: string;
   caption: string;
+  technologies?: string[];
 };
 
 const PROJECTS: Project[] = [
@@ -18,18 +20,38 @@ const PROJECTS: Project[] = [
     liveDemoUrl: "https://play-2048-with-me.web.app/",
     repoUrl: "https://github.com/jason-nordheim/react-2048",
     caption: "The classic game of combining tiles to get to 2048",
+    technologies: [
+      "Firebase",
+      "React (hooks, JSX/babel)",
+      "Yarn",
+      "JsDocs",
+      "Events (eventListeners)",
+      "JavaScript",
+      "HTML",
+    ],
   },
   {
     title: "Pure JavaScript Snake",
     liveDemoUrl: "https://vanilla-js-snake.web.app/",
     repoUrl: "https://github.com/jason-nordheim/pure-js-snake",
     caption: "Checkout the classic arcade game snake, built with CSS Grid and vanilla JavaScript",
+    technologies: ["JavaScript", "CSS", "Events (eventListeners)", "HTML"],
   },
   {
     title: "Chatter One",
     liveDemoUrl: "https://chatter-one.netlify.app/",
     repoUrl: "https://github.com/jason-nordheim/chatter-one",
     caption: "A simple chat application with a UI similar to Microsoft Teams, built using ChatEngine.io",
+    technologies: [
+      "React (hooks, JSX/babel)",
+      "Ant Icons",
+      "Axios",
+      "ChatEngine.io",
+      "localStorage",
+      "HTML",
+      "Netlify",
+      "Npm",
+    ],
   },
 ];
 
@@ -44,6 +66,8 @@ const ProjectSubtitle: FC<ProjectSubtitleProps> = ({ projectName, repoUrl, liveD
     _hover: {
       color: "blue.500",
       fontWeight: "semibold",
+      transition: "ease-in-out",
+      transitionDuration: "0.3s",
     },
   });
   return (
@@ -69,13 +93,26 @@ const ProjectSubtitle: FC<ProjectSubtitleProps> = ({ projectName, repoUrl, liveD
 
 type ProjectCardProps = ProjectSubtitleProps & {
   caption: string;
+  technologies?: string[];
 };
 
-const ProjectCard: FC<ProjectCardProps> = ({ caption, projectName, liveDemoUrl, repoUrl }) => {
+const ProjectCard: FC<ProjectCardProps> = ({ caption, projectName, liveDemoUrl, repoUrl, technologies }) => {
   return (
     <CardContainer>
       <ProjectSubtitle projectName={projectName} liveDemoUrl={liveDemoUrl} repoUrl={repoUrl} />
       <Caption>{caption}</Caption>
+      {technologies?.length ? (
+        <>
+          <Caption className={css({ fontFamily: "serif" })}>Technologies</Caption>
+          <ul className={css({ listStyle: "outside", ml: 4, fontSize: "xs", px: "10px" })}>
+            {technologies
+              .sort((a, b) => sortAscending(a, b))
+              .map((t) => (
+                <li key={t}>{t}</li>
+              ))}
+          </ul>
+        </>
+      ) : null}
     </CardContainer>
   );
 };
@@ -93,6 +130,7 @@ export const ProjectsPage = () => {
               projectName={p.title}
               liveDemoUrl={p.liveDemoUrl}
               repoUrl={p.repoUrl}
+              technologies={p.technologies}
             />
           );
         })}
