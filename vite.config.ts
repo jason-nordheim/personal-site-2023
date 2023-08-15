791684db4d698c738fd10f22175866d3e77a4212
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-15 10:11:03 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["linked-dep"],
+  },
   build: {
-    outDir: "public",
+    commonjsOptions: {
+      include: [/linked-dep/, /node_modules/],
+    },
   },
 });
